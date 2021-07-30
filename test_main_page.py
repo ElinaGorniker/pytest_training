283b46ad9f2a6f3b0740cbcdd74ca7239477a093
ElinaGorniker <elinagorniker@gmail.com>
2021-07-30 19:27:15 +0700

    add transfer

--- test_main_page.py
+++ test_main_page.py
@@ -1,32 +1,18 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open() # открываем страницу
-    page.go_to_login_page() # переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)  
+    login_page.should_be_login_page()
+    
 def test_guest_should_be_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
- 
-def test_login_url_is_correct(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = MainPage(browser, link)
-    page.open() # открываем страницу
-    page.should_be_login_url()
 
-def test_login_form_is_correct(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = MainPage(browser, link)
-    page.open() # открываем страницу
-    page.should_be_login_form()
-    
-def test_register_form_is_correct(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = MainPage(browser, link)
-    page.open() # открываем страницу
-    page.should_be_register_form()
     
