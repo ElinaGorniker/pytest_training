f2b593af229e2bda1ed7cc95da3080b60a9251a6
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2023-02-13 15:56:45 +0100

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,6 +6,7 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)  #Constructor, called every time an object is created from a class, 
-                                                         #*kwargs allows us to pass a variable number of keyword arguments to a Python function
-                                                         #*args allows us to pass a variable number of non-keyword arguments to a Python function.
+        super(MainPage, self).__init__(*args, **kwargs) #The super() function is used to give access to methods and properties of a parent or sibling class  
+                                                        #Constructor, called every time an object is created from a class, 
+                                                        #*kwargs allows us to pass a variable number of keyword arguments to a Python function
+                                                        #*args allows us to pass a variable number of non-keyword arguments to a Python function.
