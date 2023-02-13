43809f9c7748df627179cd45614051c7ddc3ca79
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2023-02-13 16:01:35 +0100

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -9,7 +9,7 @@ class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)
-        page.open() # открываем страницу
+        page.open() 
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)  
         login_page.should_be_login_page()
