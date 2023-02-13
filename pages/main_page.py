c0e66d3d07b53de1f4ad084627038bba2c100503
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2023-02-13 15:55:49 +0100

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,4 +6,6 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)  #Constructor, called every time an object is created from a class   
+        super(MainPage, self).__init__(*args, **kwargs)  #Constructor, called every time an object is created from a class, 
+                                                         #*kwargs allows us to pass a variable number of keyword arguments to a Python function
+                                                         #*args allows us to pass a variable number of non-keyword arguments to a Python function.
